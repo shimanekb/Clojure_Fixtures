48642b021871683a9253b8cbc81cf374aa02ce1c
shimanekb <brandon.shimanek@hotmail.com>
2015-09-16 18:07:08 -0500

    First commit

---
+++ fixtures/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fixtures
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ fixtures/test/fixtures/core_test.clj
@@ -0,0 +1,7 @@
+(ns fixtures.core-test
+  (:require [clojure.test :refer :all]
+            [fixtures.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
